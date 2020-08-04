8029e1a9f3e91f7c64b1828c3b1d0eaeb626cae7
Ganesan Umanesan <ganesan.umanesan@seagate.com>
2020-08-04 23:34:28 +0100

    sleep 8 plus async test.

--- scripts/c0appztest.sh
+++ scripts/c0appztest.sh
@@ -19,31 +19,46 @@ echo "Tiers!"
 set -x
 ./c0rm 21 22 -y
 ./c0cp 21 22 $TEST_FILE_256MB 1024 -x 1
-sleep 5
+sleep 8
 ./c0rm 21 23 -y
 ./c0cp 21 23 $TEST_FILE_256MB 1024 -x 2
-sleep 5
+sleep 8
 ./c0rm 21 24 -y
 ./c0cp 21 24 $TEST_FILE_256MB 1024 -x 3
+sleep 8
 set +x
 
 echo "Contiguous Mode!"
 set -x
-./c0cp 21 21 $TEST_FILE_256MB 1024 -fc 3
-sleep 5
-./c0ct 21 21 $TEST_FILE_OUT 1024 $((256*1024*1024)) -c 3
+./c0cp 21 21 $TEST_FILE_256MB 1024 -fc 2
+sleep 8
+./c0ct 21 21 $TEST_FILE_OUT 1024 $((256*1024*1024)) -c 2
+sleep 8
+./c0cp 21 21 $TEST_FILE_1GB 1024 -fc 3
+sleep 8
+./c0ct 21 21 $TEST_FILE_OUT 1024 $((1024*1024*1024)) -c 3
+sleep 8
 set +x
 
 echo "Performance Mode!"
 set -x
-./c0cp 21 21 $TEST_FILE_256MB 1024 -pfc 3
-sleep 5
-./c0ct 21 21 $TEST_FILE_OUT 1024 $((256*1024*1024)) -pc 3
-sleep 5
+./c0cp 21 21 $TEST_FILE_256MB 1024 -pfc 5
+sleep 8
+./c0ct 21 21 $TEST_FILE_OUT 1024 $((256*1024*1024)) -pc 5
+sleep 8
 ./c0cp 21 21 $TEST_FILE_1GB 1024 -pfc 3
-sleep 5
-./c0ct 21 21 $TEST_FILE_OUT 1024 $((256*1024*1024)) -pc 3
+sleep 8
+./c0ct 21 21 $TEST_FILE_OUT 1024 $((1024*1024*1024)) -pc 3
+sleep 8
+set +x
+
+echo "Asynchronous Mode!"
+set -x
+./c0cp_async 21 21 $TEST_FILE_1GB 1024 8 -pf
+sleep 8
+./c0cp_async 21 21 $TEST_FILE_1GB 1024 8 -pfc 3
+sleep 8
+./c0cp_async 21 21 $TEST_FILE_256MB 1024 8 -pfc 5
 set +x
 
 echo "DONE!"
-
