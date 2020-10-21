b097fea12a5f8cca66342519c866a7aa63cfbd19
Andriy Tkachuk <andriy.tkachuk@seagate.com>
2020-10-21 16:19:43 +0200

    Add Go bindings for Mero - mio
    `mio` Go package implements Reader/Writer interface over Mero client
    I/O API.  This allows writing Mero client applications quickly and
    efficiently in the Go language.
    
    `mio` automatically determines the optimal unit (stripe) size for the
    newly created object (based on the object size provided by user in the
    mio.Create(obj, sz) call), as well as the optimal block size for Mero
    I/O based on the cluster configuration. So users don't have to bother
    about tuning these Mero-specific parameters for each specific object to
    reach maximum I/O performance on it and yet don't waste space (in case
    of a small objects).
    
    `mio` allows to read/write the blocks to Mero in parallel threads (see
    `-threads` option) provided there is enough buffer size to accomodate
    several of such blocks in one Read()/Write() request. (For example,
    see the source code of `mcp` utility and its `-bsz` option.)
    
    `mcp` (Mero cp) utility is a client application example written in pure
    Go which uses `mio` package and has only 88 lines of code
    (as of 17 Oct 2020):
    
    ```
    Text Usage: mcp [options] src dst
    
     At least one of src and dst arguments must be object id.  The other
     can be file path or '-' for stdin/stdout.
    
      -bsz size
    	I/O buffer size (in Mbytes) (default 32)
      -ep endpoint
    	my endpoint address
      -hax endpoint
    	local hax endpoint address
      -osz size
    	object size (in Kbytes) (default 32)
      -proc fid
    	my process fid
      -prof fid
    	cluster profile fid
      -threads number
    	number of threads to use (default 1)
      -trace
    	generate m0trace.pid file
    ```
    
    To build:
    
    ```
    sh cd go/mcp && go build
    ```

---
+++ go/mcp/mcp.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+    "os"
+    "io"
+    "fmt"
+    "flag"
+    "log"
+    "../mio"
+)
+
+func usage() {
+    fmt.Fprintf(flag.CommandLine.Output(),
+`Usage: %s [options] src dst
+
+ At least one of src and dst arguments must be object id.
+ The other can be file path or '-' for stdin/stdout.
+
+`, os.Args[0])
+    flag.PrintDefaults()
+}
+
+var bufSize int
+
+func init() {
+    flag.Usage = usage
+    flag.IntVar(&bufSize, "bsz", 32, "I/O buffer `size` (in Mbytes)")
+    log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+}
+
+func main() {
+    mio.Init()
+    if flag.NArg() != 2 {
+        usage()
+        os.Exit(1)
+    }
+
+    src, dst := flag.Arg(0), flag.Arg(1)
+
+    var mioR, mioW mio.Mio
+
+    var reader io.Reader
+    if _, err := mio.ScanID(src); err == nil {
+        if err = mioR.Open(src); err != nil {
+            log.Fatalf("failed to open object %v: %v", src, err)
+        }
+        defer mioR.Close()
+        reader = &mioR
+    } else if src == "-" {
+        reader = os.Stdin
+    } else {
+        file, err := os.Open(src)
+        if err != nil {
+            log.Fatalf("failed to open file %v: %v", src, err)
+        }
+        defer file.Close()
+        info, err := file.Stat()
+        if err != nil {
+            log.Fatalf("failed to get stat of file %v: %v", src, err)
+        }
+        mio.ObjSize = uint64(info.Size())
+        reader = file
+    }
+
+    var writer io.Writer
+    if _, err := mio.ScanID(dst); err == nil {
+        if err = mioW.Open(dst); err != nil {
+            if err = mioW.Create(dst, mio.ObjSize); err != nil {
+                log.Fatalf("failed to create object %v: %v", dst, err)
+            }
+        }
+        defer mioW.Close()
+        writer = &mioW
+    } else if dst == "-" {
+        writer = os.Stdout
+    } else {
+        file, err := os.OpenFile(dst, os.O_WRONLY|os.O_CREATE, 0655)
+        if err != nil {
+            log.Fatalf("failed to open file %v: %v", dst, err)
+        }
+        defer file.Close()
+        writer = file
+    }
+
+    buf := make([]byte, bufSize * 1024 * 1024)
+    // always use buf, hack from https://github.com/golang/go/issues/16474
+    io.CopyBuffer(struct{ io.Writer }{writer}, struct{ io.Reader }{reader}, buf)
+}
