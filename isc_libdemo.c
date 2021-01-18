d456eae55629a909c3590f0e56bdd42c46ddd695
Andriy Tkachuk <andriy.tkachuk@seagate.com>
2021-01-18 15:57:06 +0000

    Update license in isc_libdemo.c
    Signed-off-by: Andriy Tkachuk <andriy.tkachuk@seagate.com>

--- isc_libdemo.c
+++ isc_libdemo.c
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2018-2020 Seagate Technology LLC and/or its Affiliates
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For any questions about this software or licensing,
+ * please email opensource@seagate.com or cortx-questions@seagate.com.
+ *
+ * Original author:  Nachiket Sahasrabuddhe <nachiket.sahasrabuddhe@seagate.com>
+ * Original creation date: 06-Sep-2018
+ */
+
 #include "lib/misc.h"       /* m0_full_name_hash */
 #include "fid/fid.h"        /* m0_fid */
 #include "lib/buf.h"        /* m0_buf */
