Seagate/cortx-motr-apps

commit da553887292101f8d682db791ccb988937d75767

isc: optimize min/max computation
Signed-off-by: Andriy Tkachuk <andriy.tkachuk@seagate.com>

c0isc_demo.c
@@ -45,20 +45,6 @@ enum isc_comp_type {
 	ICT_MAX,
 };
 
-/** Arguments for min/max operations */
-struct mm_args {
-	/** Length of an array. */
-	uint32_t  ma_len;
-	/** Array of doubles. */
-	double   *ma_arr;
-	/** Number of isc services. */
-	uint32_t  ma_svc_nr;
-	/** Length of a chunk per service. */
-	uint32_t  ma_chunk_len;
-	/** A service currently fed with input. */
-	uint32_t  ma_curr_svc_id;
-};
-
 static void fid_get(const char *f_name, struct m0_fid *fid)
 {
 	uint32_t f_key = m0_full_name_hash((const unsigned char*)f_name,
@@ -84,37 +70,6 @@ static int op_type_parse(const char *op_name)
 
 }
 
-static int file_to_array(const char *file_name, void **arr, uint32_t *arr_len)
-{
-	FILE     *fd;
-	uint32_t  i;
-	int       rc;
-	double   *val_arr;
-
-	fd = fopen(file_name, "r");
-	if (fd == NULL) {
-		fprintf(stderr, "error! Could not open file c0isc_data\n");
-		return -EINVAL;
-	}
-	fscanf(fd, "%d", arr_len);
-	/* XXX: Fix sizeof (double) with appropriate macro. */
-	M0_ALLOC_ARR(val_arr, *arr_len);
-	for (i = 0; i < *arr_len; ++i) {
-		rc = fscanf(fd, "%lf", &val_arr[i]);
-		if (rc == EOF) {
-			fprintf(stderr, "data file (%s) does not contain the "
-				"specified number of elements: %d\n",
-				file_name, *arr_len);
-			m0_free(val_arr);
-			fclose(fd);
-			return -EINVAL;
-		}
-	}
-	*arr = val_arr;
-	fclose(fd);
-	return 0;
-}
-
 static uint32_t isc_services_count(void)
 {
 	struct m0_fid start_fid = M0_FID0;
@@ -130,52 +85,6 @@ static uint32_t isc_services_count(void)
 		start_fid = proc_fid;
 	}
 	return svc_nr;
-}
-
-static int op_init(enum isc_comp_type type, void **inp_args)
-{
-	struct mm_args *in_info;
-	double         *arr;
-	uint32_t        arr_len;
-	int             rc;
-
-	switch (type) {
-	case ICT_PING:
-		return 0;
-	case ICT_MIN:
-	case ICT_MAX:
-		M0_ALLOC_PTR(in_info);
-		if (in_info == NULL)
-			return -ENOMEM;
-		rc = file_to_array("c0isc_data", (void **)&arr, &arr_len);
-		if (rc != 0)
-			return rc;
-		in_info->ma_arr = arr;
-		in_info->ma_len = arr_len;
-		in_info->ma_curr_svc_id = 0;
-		in_info->ma_svc_nr = isc_services_count();
-		in_info->ma_chunk_len = arr_len / in_info->ma_svc_nr;
-		*inp_args = in_info;
-
-		return 0;
-	default:
-		fprintf(stderr, "Invalid operation %d\n", type);
-		return EINVAL;
-	}
-}
-
-static void op_fini(enum isc_comp_type op_type, struct mm_args *in_info)
-{
-	switch (op_type) {
-	case ICT_PING:
-		break;
-	case ICT_MIN:
-	case ICT_MAX:
-		if (in_info == NULL)
-			break;
-		m0_free(in_info->ma_arr);
-		m0_free(in_info);
-	}
 }
 
 static int minmax_input_prepare(struct m0_buf *out, struct m0_fid *comp_fid,
@@ -205,9 +114,9 @@ static int minmax_input_prepare(struct m0_buf *out, struct m0_fid *comp_fid,
 	m0_buf_free(&buf);
 
 	if (type == ICT_MIN)
-		fid_get("arr_min", comp_fid);
+		fid_get("comp_min", comp_fid);
 	else
-		fid_get("arr_max", comp_fid);
+		fid_get("comp_max", comp_fid);
 
 	*reply_len = CBL_DEFAULT_MAX;
 
@@ -468,14 +377,11 @@ int launch_comp(struct m0_layout_plan *plan, int op_type, bool last)
 	struct m0_layout_plop *plop = NULL;
 	struct m0_layout_plop *prev_plop;
 	struct m0_layout_io_plop *iopl;
-	void                  *inp_args = NULL; /* computation input args */
 	static void           *out_args = NULL; /* computation output */
 	const char            *conn_addr = NULL;
 	struct m0_fid          comp_fid;
 	struct m0_buf          buf;
 
-	/* Initialise the  parameters for operation. */
-	rc = op_init(op_type, &inp_args);
 	while (rc == 0) {
 		M0_ALLOC_PTR(req);
 		if (req == NULL) {
@@ -551,8 +457,6 @@ int launch_comp(struct m0_layout_plan *plan, int op_type, bool last)
 		c0appz_isc_req_fini(req);
 		m0_free(req);
 	}
-
-	op_fini(op_type, inp_args);
 
 	return rc;
 }

isc_libdemo.c
@@ -223,26 +223,20 @@ int launch_stob_io(struct m0_isc_comp_private *pdata,
 	return M0_FSO_WAIT;
 }
 
-static int buf_to_array(char *buf, double **arr,
-			struct m0_buf *lbuf, struct m0_buf *rbuf)
+int compute_minmax(enum op op, struct m0_isc_comp_private *pdata,
+		   struct m0_buf *out, int *rc)
 {
-	int         i;
-	int         n;
-	int         rc;
-	int         arr_len = 0;
-	double      val;
-	double     *val_arr;
-	const char *p = buf;
+	int               i;
+	int               n;
+	char             *p;
+	double            val;
+	struct mm_result  res;
+	struct m0_buf     buf = M0_BUF_INIT0;
+	struct m0_stob_io *stio = (struct m0_stob_io *)pdata->icp_data;
 
-	/* calc number of lines 1st to estimate the array length */
-	while (sscanf(p, "%lf\n%n", &val, &n) > 0) {
-		arr_len++;
-		p += n;
-	}
+	bufvec_open(&stio->si_user, m0_stob_block_shift(stio->si_obj));
 
-	M0_ALLOC_ARR(val_arr, arr_len);
-	if (val_arr == NULL)
-		return M0_ERR(-ENOMEM);
+	p = stio->si_user.ov_buf[0];
 
 	/*
 	 * 1st value should go to the lbuf always, because it can be
@@ -250,18 +244,24 @@ static int buf_to_array(char *buf, double **arr,
 	 *
 	 * The same is true for the last value.
 	 */
-	rc = sscanf(buf, "%lf\n%n", &val, &n);
-	if (rc < 1)
-		return M0_ERR(-EINVAL);
-	lbuf->b_addr = buf;
-	lbuf->b_nob = n;
-	buf += n;
+	if (sscanf(p, "%lf\n%n", &val, &n) < 1) {
+		*rc = M0_ERR(-EINVAL);
+		return M0_FSO_AGAIN;
+	}
+	res.mr_lbuf.b_addr = p;
+	res.mr_lbuf.b_nob = n;
+	p += n;
 
-	for (i = 0; i < arr_len; ++i) {
-		rc = sscanf(buf, "%lf\n%n", &val_arr[i], &n);
-		if (rc < 1)
-			break;
-		buf += n;
+	res.mr_idx = 0;
+	res.mr_val = 0;
+
+	for (i = 0; sscanf(p, "%lf\n%n", &val, &n) > 0; i++) {
+		if (op == MIN ? val < res.mr_val :
+		                val > res.mr_val) {
+			res.mr_idx = i;
+			res.mr_val = val;
+		}
+		p += n;
 	}
 
 	/*
@@ -269,50 +269,16 @@ static int buf_to_array(char *buf, double **arr,
 	 * the left cut of the first value of the next unit.
 	 */
 	if (i > 0) {
-		rbuf->b_addr = buf - n;
-		rbuf->b_nob = n;
+		res.mr_rbuf.b_addr = p - n;
+		res.mr_rbuf.b_nob = n;
 		i--;
 	}
 
-	*arr = val_arr;
-	return i;
-}
-
-int compute_minmax(enum op op, struct m0_isc_comp_private *pdata,
-		   struct m0_buf *out, int *rc)
-{
-	int               i;
-	int               arr_len;
-	double           *arr;
-	struct mm_result  curr;
-	struct m0_buf     buf = M0_BUF_INIT0;
-	struct m0_stob_io *stio = (struct m0_stob_io *)pdata->icp_data;
-
-	bufvec_open(&stio->si_user, m0_stob_block_shift(stio->si_obj));
-
-	arr_len = buf_to_array(stio->si_user.ov_buf[0], &arr,
-			       &curr.mr_lbuf, &curr.mr_rbuf);
-	if (arr_len < 0) {
-		*rc = arr_len;
-		return M0_FSO_AGAIN;
-	}
-
-	curr.mr_idx = 0;
-	curr.mr_val = arr[0];
-
-	for (i = 1; i < arr_len; ++i) {
-		if (op == MIN ? arr[i] < curr.mr_val :
-		                arr[i] > curr.mr_val) {
-			curr.mr_idx = i;
-			curr.mr_val = arr[i];
-		}
-	}
-
 	/* +1 for the lbuf element not included in the array */
-	curr.mr_idx++;
-	curr.mr_idx_max = arr_len + 1;
+	res.mr_idx++;
+	res.mr_idx_max = i + 1;
 
-	*rc = m0_xcode_obj_enc_to_buf(&M0_XCODE_OBJ(mm_result_xc, &curr),
+	*rc = m0_xcode_obj_enc_to_buf(&M0_XCODE_OBJ(mm_result_xc, &res),
 				      &buf.b_addr, &buf.b_nob) ?:
 	      m0_buf_copy_aligned(out, &buf, M0_0VEC_SHIFT);
 
@@ -321,8 +287,8 @@ int compute_minmax(enum op op, struct m0_isc_comp_private *pdata,
 	return M0_FSO_AGAIN;
 }
 
-int arr_minmax(enum op op, struct m0_buf *in, struct m0_buf *out,
-	       struct m0_isc_comp_private *data, int *rc)
+int do_minmax(enum op op, struct m0_buf *in, struct m0_buf *out,
+	      struct m0_isc_comp_private *data, int *rc)
 {
 	int                res;
 	struct m0_stob_io *stio = (struct m0_stob_io *)data->icp_data;
@@ -346,16 +312,16 @@ int arr_minmax(enum op op, struct m0_buf *in, struct m0_buf *out,
 	return res;
 }
 
-int arr_min(struct m0_buf *in, struct m0_buf *out,
-	    struct m0_isc_comp_private *comp_data, int *rc)
+int comp_min(struct m0_buf *in, struct m0_buf *out,
+	     struct m0_isc_comp_private *comp_data, int *rc)
 {
-	return arr_minmax(MIN, in, out, comp_data, rc);
+	return do_minmax(MIN, in, out, comp_data, rc);
 }
 
-int arr_max(struct m0_buf *in, struct m0_buf *out,
-	    struct m0_isc_comp_private *comp_data, int *rc)
+int comp_max(struct m0_buf *in, struct m0_buf *out,
+	     struct m0_isc_comp_private *comp_data, int *rc)
 {
-	return arr_minmax(MAX, in, out, comp_data, rc);
+	return do_minmax(MAX, in, out, comp_data, rc);
 }
 
 static void comp_reg(const char *f_name, int (*ftn)(struct m0_buf *arg_in,
@@ -377,7 +343,7 @@ static void comp_reg(const char *f_name, int (*ftn)(struct m0_buf *arg_in,
 void motr_lib_init(void)
 {
 	comp_reg("hello_world", hello_world);
-	comp_reg("arr_min", arr_min);
-	comp_reg("arr_max", arr_max);
+	comp_reg("comp_min", comp_min);
+	comp_reg("comp_max", comp_max);
 	m0_xc_isc_libdemo_init();
 }

c0isc_data
@@ -1,4 +1,0 @@
-3
-1
-2
-3

commit 47752be9be3d0db0366de1c70085200df0c5d762

isc: fix min calc regression after da55388
Signed-off-by: Andriy Tkachuk <andriy.tkachuk@seagate.com>

c0appz.c
@@ -903,16 +903,16 @@ int c0appz_isc_nxt_svc_get(struct m0_fid *svc_fid, struct m0_fid *nxt_fid,
 
 int c0appz_isc_req_prepare(struct c0appz_isc_req *req, struct m0_buf *args,
 			   const struct m0_fid *comp_fid,
-			   struct m0_layout_io_plop *iopl, uint32_t reply_len)
+			   struct m0_layout_io_plop *iop, uint32_t reply_len)
 {
-	struct m0_rpc_session *sess = iopl->iop_session;
+	struct m0_rpc_session *sess = iop->iop_session;
 	struct m0_fop_isc  *fop_isc = &req->cir_isc_fop;
 	struct m0_fop      *arg_fop = &req->cir_fop;
 	int                 rc;
 
-	req->cir_plop = &iopl->iop_base;
-	req->cir_args = args;
+	req->cir_plop = &iop->iop_base;
 	fop_isc->fi_comp_id = *comp_fid;
+	fop_isc->fi_cob = iop->iop_base.pl_ent;
 	m0_rpc_at_init(&fop_isc->fi_args);
 	rc = m0_rpc_at_add(&fop_isc->fi_args, args, sess->s_conn);
 	if (rc != 0) {

c0appz_isc.h
@@ -35,8 +35,6 @@ enum c0appz_buffer_len {
 
 /** A request holding all parameters relevant to a computation. */
 struct c0appz_isc_req {
-	/** Arguments for computation. */
-	struct m0_buf         *cir_args;
 	/** Buffer to store returned result. */
 	struct m0_buf          cir_result;
 	/** Error code for the computation. */

c0isc_demo.c
@@ -382,7 +382,7 @@ int launch_comp(struct m0_layout_plan *plan, int op_type, bool last)
 	struct m0_fid          comp_fid;
 	struct m0_buf          buf;
 
-	while (rc == 0) {
+	for (;;) {
 		M0_ALLOC_PTR(req);
 		if (req == NULL) {
 			fprintf(stderr, "request allocation failed\n");

isc_libdemo.c
@@ -226,17 +226,19 @@ int launch_stob_io(struct m0_isc_comp_private *pdata,
 int compute_minmax(enum op op, struct m0_isc_comp_private *pdata,
 		   struct m0_buf *out, int *rc)
 {
-	int               i;
+	int               i = 0;
 	int               n;
-	char             *p;
+	uint32_t          shift;
+	char             *p, *end;
 	double            val;
-	struct mm_result  res;
+	struct mm_result  res = {};
 	struct m0_buf     buf = M0_BUF_INIT0;
 	struct m0_stob_io *stio = (struct m0_stob_io *)pdata->icp_data;
 
-	bufvec_open(&stio->si_user, m0_stob_block_shift(stio->si_obj));
-
+	shift = m0_stob_block_shift(stio->si_obj);
+	bufvec_open(&stio->si_user, shift);
 	p = stio->si_user.ov_buf[0];
+	end = p + (stio->si_count << shift);
 
 	/*
 	 * 1st value should go to the lbuf always, because it can be
@@ -252,29 +254,29 @@ int compute_minmax(enum op op, struct m0_isc_comp_private *pdata,
 	res.mr_lbuf.b_nob = n;
 	p += n;
 
-	res.mr_idx = 0;
-	res.mr_val = 0;
+	/* read 1st element to compare with */
+	if (sscanf(p, "%lf\n%n", &val, &n) < 1)
+		goto out;
+	p += n;
+	res.mr_val = val;
 
-	for (i = 0; sscanf(p, "%lf\n%n", &val, &n) > 0; i++) {
+	for (i = 1; p < end && sscanf(p, "%lf\n%n", &val, &n) > 0 &&
+	            p + n < end; i++, p += n) {
 		if (op == MIN ? val < res.mr_val :
 		                val > res.mr_val) {
 			res.mr_idx = i;
 			res.mr_val = val;
 		}
-		p += n;
 	}
-
 	/*
 	 * Last value should go to the rbuf always, because it can be
-	 * the left cut of the first value of the next unit.
+	 * the left cut of the first value of the next unit. rbuf element
+	 * is counted as lbuf on the next one.
 	 */
-	if (i > 0) {
-		res.mr_rbuf.b_addr = p - n;
-		res.mr_rbuf.b_nob = n;
-		i--;
-	}
-
-	/* +1 for the lbuf element not included in the array */
+	res.mr_rbuf.b_addr = p;
+	res.mr_rbuf.b_nob = n;
+ out:
+	/* +1 for the lbuf element */
 	res.mr_idx++;
 	res.mr_idx_max = i + 1;
 