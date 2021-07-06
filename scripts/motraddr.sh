5397c7c5be9c0521d23a111cd26f750c3f73262c
steven <wdchien@kth.se>
2021-07-06 12:48:11 +0200

    improvements
    - remove duplicated local address extraction
    - use shell parameter expansion to combine HA address
    
    Signed-off-by: steven <wdchien@kth.se>

--- scripts/motraddr.sh
+++ scripts/motraddr.sh
@@ -43,11 +43,9 @@ p[1]=$(grep -A2 Profile $hastatus | tail -n1 | awk '{print $1}')
 p[2]=$(sed -n '/pools:/,/^[A-Za-z]/p' $hastatus | grep -E 'tier.+p1' | awk '{print $1}')
 p[3]=$(sed -n '/pools:/,/^[A-Za-z]/p' $hastatus | grep -E 'tier.+p2' | awk '{print $1}')
 p[4]=$(sed -n '/pools:/,/^[A-Za-z]/p' $hastatus | grep -E 'tier.+p3' | awk '{print $1}')
-p[5]=$(grep -A$((2+($r)%16)) $c $hastatus | tail -n1 | awk '{print $4}')
 [[ -z "${p[2]}" ]] && { echo "Error: M0_POOL_TIER1 not found"; exit 1; }
 [[ -z "${p[3]}" ]] && { echo "Error: M0_POOL_TIER2 not found"; exit 1; }
 [[ -z "${p[4]}" ]] && { echo "Error: M0_POOL_TIER3 not found"; exit 1; }
-[[ -z "${p[5]}" ]] && { echo "Error: LOCAL_ENDPOINT_ADDR0 not found"; exit 1; }
 
 # LOCAL_ENDPOINT_ADDR0
 p[5]=$(grep -A$((2+($r)%16)) $c $hastatus | tail -n1 | awk '{print $4}')
@@ -169,7 +167,7 @@ echo "# Application: All"
 echo "#"
 
 echo
-echo "HA_ENDPOINT_ADDR = $(echo "${p[5]}" | cut -d'@' -f 1)@$(echo "${p[0]}" | cut -d'@' -f 2)"
+echo "HA_ENDPOINT_ADDR = ${p[5]%@*}@${p[0]#*@}"
 echo "PROFILE_FID = ${p[1]}"
 
 echo
