778429fb5d39e06afa316b36d04ae535e3be4aaf
Ganesan Umanesan <519680@emr-u519680l001.ad.seagate.com>
2021-11-11 12:58:21 +0000

    improved cleanup to remove objects on exit

--- scripts/basic.sh
+++ scripts/basic.sh
@@ -6,24 +6,26 @@ FILE2=$(mktemp --tmpdir file2.XXXXXX)
 cleanup()
 {
 	rm -f $FILE1 $FILE2
+	( set -x; sleep 0.5; ./c0rm $id1 -y $1 )	
 }
 
 trap cleanup EXIT
 
 echo 'Generating test files ...'
-dd if=/dev/urandom of=$FILE1 bs=8M count=128
+dd if=/dev/urandom of=$FILE1 bs=8M count=4
+ls -lh $FILE1
 
-bsz=$((1*1024))
-fsz=$((1024*1024*1024))
+bsz=$((1*64))
+fsz=$(wc -c < "$FILE1")
 id1=$(./fgen);
 
 set -x
 # write
-sleep 0.5; ./c0cp $id1 $FILE1 $bsz -b -pf -c1 $1
+sleep 0.5; ./c0cp $id1 $FILE1 $bsz -b -pf -mc1 $1
 # write (async)
-sleep 0.5; ./c0cp $id1 $FILE1 $bsz -b -pf -c1 -a8 $1
+sleep 0.5; ./c0cp $id1 $FILE1 $bsz -b -pf -mc1 -a8 $1
 # read
-sleep 0.5; ./c0cat $id1 $FILE2 $bsz $fsz -b -p -c1 $1
-# delete
-sleep 0.5; ./c0rm $id1 -y $1
+sleep 0.5; ./c0cat $id1 $FILE2 $bsz $fsz -b -p -mc1 $1
+# compare
+diff $FILE1 $FILE2
 set +x
