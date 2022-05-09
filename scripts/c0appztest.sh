84304f91e69dcab539f32110828cedee4381c3d0
Ganesan Umanesan <ganesan.umanesan@seagate.com>
2022-05-09 10:18:07 +0100

    codacy double quote fix

--- scripts/c0appztest.sh
+++ scripts/c0appztest.sh
@@ -6,72 +6,72 @@ TEST_FILE_OUT=$(mktemp --tmpdir out.XXXXXX)
 
 cleanup()
 {
-	rm -f $TEST_FILE_256MB $TEST_FILE_1GB $TEST_FILE_OUT
+	rm -f "$TEST_FILE_256MB" "$TEST_FILE_1GB" "$TEST_FILE_OUT"
 }
 
 trap cleanup EXIT
 
 echo 'Generating test files ...'
-dd if=/dev/urandom of=$TEST_FILE_256MB bs=1M count=256
-dd if=/dev/urandom of=$TEST_FILE_1GB bs=8M count=128
+dd if=/dev/urandom of="$TEST_FILE_256MB" bs=1M count=256
+dd if=/dev/urandom of="$TEST_FILE_1GB" bs=8M count=128
 
 echo "Tiers!"
 set -x
 ./c0rm 21 10 -y
-./c0cp 21 10 $TEST_FILE_256MB 1024 -x 1
+./c0cp 21 10 "$TEST_FILE_256MB" 1024 -x 1
 ./c0rm 21 20 -y
-./c0cp 21 20 $TEST_FILE_256MB 1024 -x 2
+./c0cp 21 20 "$TEST_FILE_256MB" 1024 -x 2
 ./c0rm 21 30 -y
-./c0cp 21 30 $TEST_FILE_256MB 1024 -x 3
+./c0cp 21 30 "$TEST_FILE_256MB" 1024 -x 3
 set +x
 
 echo "Contiguous Mode!"
 set -x
 ./c0rm 21 21 -y
-./c0cp 21 21 $TEST_FILE_256MB 1024 -fc 2
-./c0cat 21 21 $TEST_FILE_OUT 1024 $((256*1024*1024)) -c 2
-./c0cp 21 21 $TEST_FILE_1GB 1024 -fc 3
-./c0cat 21 21 $TEST_FILE_OUT 1024 $((1024*1024*1024)) -c 3
+./c0cp 21 21 "$TEST_FILE_256MB" 1024 -fc 2
+./c0cat 21 21 "$TEST_FILE_OUT" 1024 $((256*1024*1024)) -c 2
+./c0cp 21 21 "$TEST_FILE_1GB" 1024 -fc 3
+./c0cat 21 21 "$TEST_FILE_OUT" 1024 $((1024*1024*1024)) -c 3
 set +x
 
 echo "Performance Mode!"
 set -x
 #default tier
-./c0cp 21 21 $TEST_FILE_256MB 1024 -pfc 5
-./c0cat 21 21 $TEST_FILE_OUT 1024 $((256*1024*1024)) -pc 5
-./c0cp 21 21 $TEST_FILE_1GB 1024 -pfc 3
-./c0cat 21 21 $TEST_FILE_OUT 1024 $((1024*1024*1024)) -pc 3
+./c0cp 21 21 "$TEST_FILE_256MB" 1024 -pfc 5
+./c0cat 21 21 "$TEST_FILE_OUT" 1024 $((256*1024*1024)) -pc 5
+./c0cp 21 21 "$TEST_FILE_1GB" 1024 -pfc 3
+./c0cat 21 21 "$TEST_FILE_OUT" 1024 $((1024*1024*1024)) -pc 3
 #tiers 123
 ./c0rm 21 221 -y
 ./c0rm 21 222 -y
 ./c0rm 21 223 -y
-./c0cp 21 221 $TEST_FILE_1GB 1024 -x 1 -p
-./c0cp 21 222 $TEST_FILE_1GB 1024 -x 2 -p
-./c0cp 21 223 $TEST_FILE_1GB 1024 -x 3 -p
-./c0cp 21 221 $TEST_FILE_1GB 1024 -x 1 -pfc 3
-./c0cp 21 222 $TEST_FILE_1GB 1024 -x 2 -pfc 3
-./c0cp 21 223 $TEST_FILE_1GB 1024 -x 3 -pfc 3
-./c0cat 21 221 $TEST_FILE_OUT 1024 $((1024*1024*1024)) -p
-./c0cat 21 222 $TEST_FILE_OUT 1024 $((1024*1024*1024)) -p
-./c0cat 21 223 $TEST_FILE_OUT 1024 $((1024*1024*1024)) -p
-./c0cat 21 221 $TEST_FILE_OUT 1024 $((1024*1024*1024)) -pc 3
-./c0cat 21 222 $TEST_FILE_OUT 1024 $((1024*1024*1024)) -pc 3
-./c0cat 21 223 $TEST_FILE_OUT 1024 $((1024*1024*1024)) -pc 3
+./c0cp 21 221 "$TEST_FILE_1GB" 1024 -x 1 -p
+./c0cp 21 222 "$TEST_FILE_1GB" 1024 -x 2 -p
+./c0cp 21 223 "$TEST_FILE_1GB" 1024 -x 3 -p
+./c0cp 21 221 "$TEST_FILE_1GB" 1024 -x 1 -pfc 3
+./c0cp 21 222 "$TEST_FILE_1GB" 1024 -x 2 -pfc 3
+./c0cp 21 223 "$TEST_FILE_1GB" 1024 -x 3 -pfc 3
+./c0cat 21 221 "$TEST_FILE_OUT" 1024 $((1024*1024*1024)) -p
+./c0cat 21 222 "$TEST_FILE_OUT" 1024 $((1024*1024*1024)) -p
+./c0cat 21 223 "$TEST_FILE_OUT" 1024 $((1024*1024*1024)) -p
+./c0cat 21 221 "$TEST_FILE_OUT" 1024 $((1024*1024*1024)) -pc 3
+./c0cat 21 222 "$TEST_FILE_OUT" 1024 $((1024*1024*1024)) -pc 3
+./c0cat 21 223 "$TEST_FILE_OUT" 1024 $((1024*1024*1024)) -pc 3
 set +x
 
 echo "Asynchronous Mode!"
 set -x
 #default tier
-./c0cp 21 21 $TEST_FILE_1GB 1024 -a 8 -pf
-./c0cp 21 21 $TEST_FILE_1GB 1024 -a 8 -pfc 3
-./c0cp 21 21 $TEST_FILE_256MB 1024 -a 8 -pfc 5
+./c0cp 21 21 "$TEST_FILE_1GB" 1024 -a 8 -pf
+./c0cp 21 21 "$TEST_FILE_1GB" 1024 -a 8 -pfc 3
+./c0cp 21 21 "$TEST_FILE_256MB" 1024 -a 8 -pfc 5
 #tiers 123 
-./c0cp 21 221 $TEST_FILE_1GB 1024 -a 8 -x 1 -p
-./c0cp 21 222 $TEST_FILE_1GB 1024 -a 8 -x 2 -p
-./c0cp 21 223 $TEST_FILE_1GB 1024 -a 8 -x 3 -p
-./c0cp 21 221 $TEST_FILE_1GB 1024 -a 8 -x 1 -pfc 3
-./c0cp 21 222 $TEST_FILE_1GB 1024 -a 8 -x 2 -pfc 3
-./c0cp 21 223 $TEST_FILE_1GB 1024 -a 8 -x 3 -pfc 3
+./c0cp 21 221 "$TEST_FILE_1GB" 1024 -a 8 -x 1 -p
+./c0cp 21 222 "$TEST_FILE_1GB" 1024 -a 8 -x 2 -p
+./c0cp 21 223 "$TEST_FILE_1GB" 1024 -a 8 -x 3 -p
+./c0cp 21 221 "$TEST_FILE_1GB" 1024 -a 8 -x 1 -pfc 3
+./c0cp 21 222 "$TEST_FILE_1GB" 1024 -a 8 -x 2 -pfc 3
+./c0cp 21 223 "$TEST_FILE_1GB" 1024 -a 8 -x 3 -pfc 3
 set +x
 
 echo "DONE!"
--- scripts/tiertest.sh
+++ scripts/tiertest.sh
@@ -5,7 +5,7 @@ FILE2=$(mktemp --tmpdir file2.XXXXXX)
 
 cleanup()
 {
-	rm -f $FILE1 $FILE2
+	rm -f "$FILE1" "$FILE2"
 }
 
 trap cleanup EXIT
@@ -21,19 +21,19 @@ id3=$(./fgen);
 
 set -x
 # write
-sleep 0.5; ./c0cp $id1 $FILE1 $bsz -b -px1 -c1
-sleep 0.5; ./c0cp $id2 $FILE1 $bsz -b -px2 -c1
-sleep 0.5; ./c0cp $id3 $FILE1 $bsz -b -px3 -c1
+sleep 0.5; ./c0cp "$id1" "$FILE1" "$bsz" -b -px1 -c1
+sleep 0.5; ./c0cp "$id2" "$FILE1" "$bsz" -b -px2 -c1
+sleep 0.5; ./c0cp "$id3" "$FILE1" "$bsz" -b -px3 -c1
 # write (async)
-sleep 0.5; ./c0cp $id1 $FILE1 $bsz -b -pfx1 -c1 -a8
-sleep 0.5; ./c0cp $id2 $FILE1 $bsz -b -pfx2 -c1 -a8
-sleep 0.5; ./c0cp $id3 $FILE1 $bsz -b -pfx3 -c1 -a8
+sleep 0.5; ./c0cp "$id1" "$FILE1" "$bsz" -b -pfx1 -c1 -a8
+sleep 0.5; ./c0cp "$id2" "$FILE1" "$bsz" -b -pfx2 -c1 -a8
+sleep 0.5; ./c0cp "$id3" "$FILE1" "$bsz" -b -pfx3 -c1 -a8
 # read
-sleep 0.5; ./c0cat $id1 $FILE2 $bsz $fsz -b -p -c1
-sleep 0.5; ./c0cat $id2 $FILE2 $bsz $fsz -b -p -c1
-sleep 0.5; ./c0cat $id3 $FILE2 $bsz $fsz -b -p -c1
+sleep 0.5; ./c0cat "$id1" "$FILE2" "$bsz" "$fsz" -b -p -c1
+sleep 0.5; ./c0cat "$id2" "$FILE2" "$bsz" "$fsz" -b -p -c1
+sleep 0.5; ./c0cat "$id3" "$FILE2" "$bsz" "$fsz" -b -p -c1
 # delete
-sleep 0.5; ./c0rm $id1 -y 
-sleep 0.5; ./c0rm $id2 -y 
-sleep 0.5; ./c0rm $id3 -y 
+sleep 0.5; ./c0rm "$id1" -y 
+sleep 0.5; ./c0rm "$id2" -y 
+sleep 0.5; ./c0rm "$id3" -y 
 set +x
