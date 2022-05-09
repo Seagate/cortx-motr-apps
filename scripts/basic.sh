66def50747f5e29f2f3566a5ffdf9185159ef09e
Ganesan Umanesan <ganesan.umanesan@seagate.com>
2022-05-09 10:18:07 +0100

    codacy double quote fix

--- scripts/basic.sh
+++ scripts/basic.sh
@@ -5,15 +5,15 @@ FILE2=$(mktemp --tmpdir file2.XXXXXX)
 
 cleanup()
 {
-	rm -f $FILE1 $FILE2
-	( set -x; sleep 0.5; ./c0rm $id1 -y $1 )	
+	rm -f "$FILE1" "$FILE2"
+	( set -x; sleep 0.5; ./c0rm ${id1[0]} ${id1[1]} -y $1 )	
 }
 
 trap cleanup EXIT
 
 echo 'Generating test files ...'
-dd if=/dev/urandom of=$FILE1 bs=8M count=4
-ls -lh $FILE1
+dd if=/dev/urandom of="$FILE1" bs=8M count=4
+ls -lh "$FILE1"
 
 bsz=$((1*64))
 fsz=$(wc -c < "$FILE1")
@@ -21,11 +21,11 @@ id1=$(./fgen);
 
 set -x
 # write
-sleep 0.5; ./c0cp $id1 $FILE1 $bsz -b -pf -mc1 $1
+sleep 0.5; ./c0cp ${id1[0]} ${id1[1]} "$FILE1" "$bsz" -b -pf -mc1 $1
 # write (async)
-sleep 0.5; ./c0cp $id1 $FILE1 $bsz -b -pf -mc1 -a8 $1
+sleep 0.5; ./c0cp ${id1[0]} ${id1[1]} "$FILE1" "$bsz" -b -pf -mc1 -a8 $1
 # read
-sleep 0.5; ./c0cat $id1 $FILE2 $bsz $fsz -b -p -mc1 $1
+sleep 0.5; ./c0cat ${id1[0]} ${id1[1]} "$FILE2" "$bsz" "$fsz" -b -p -mc1 $1
 # compare
-diff $FILE1 $FILE2
+diff "$FILE1" "$FILE2"
 set +x
